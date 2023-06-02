05cc504bfb7ed905d99af58ed8f9b655c05820e0
Syahreza777 <125122374+Syahreza777@users.noreply.github.com>
2023-06-02 07:21:54 -0700

    Add files via upload

---
+++ mini-project-1/modules/transactions/entitiy.go
@@ -0,0 +1,45 @@
+package transactions
+
+import (
+	"errors"
+	"mini-project/modules/products"
+	"time"
+)
+
+type Transaction struct {
+	ID        int               `gorm:"primaryKey" json:"id"`
+	Timestamp time.Time         `json:"timestamp"`
+	Total     int               `gorm:"not null" json:"total"`
+	AdminID   float64           `json:"admin_id"`
+	Admin     User              `json:"admin"`
+	Items     []TransactionItem `json:"items"`
+}
+
+type User struct {
+	ID   int
+	Name string
+}
+
+type TransactionItem struct {
+	ID            int               `gorm:"primaryKey" json:"id"`
+	TransactionID uint              `gorm:"not null;foreignKey:TransactionID"`
+	ProductID     uint              `gorm:"not null;foreignKey:ProductID"`
+	Quantity      int               `gorm:"not null" json:"quantity"`
+	Price         int               `gorm:"not null" json:"price"`
+	Product       *products.Product `json:"product"`
+}
+
+type CreateItemRequest struct {
+	ProductID int `json:"product_id"`
+	Quantity  int `json:"quantity"`
+}
+
+type CreateTransactionRequest struct {
+	Items []CreateItemRequest
+}
+
+var (
+	ErrProductIdNotFound    = errors.New("Product id not found")
+	ErrStockNotEnough       = errors.New("Stock not enough")
+	ErrPoductHasBeenRemoved = errors.New("Product has been removed")
+)
